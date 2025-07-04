4888ee170c19ba3747ed0cbfbac134647195898b
Itz_MIles <95124554+Itz-Miles@users.noreply.github.com>
2025-07-04 11:53:11 -0600

    build workflow (with caching)
    Co-authored-by: mrchaoss1 <mrchaoss1@users.noreply.github.com>

--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -5,8 +5,8 @@ cd ..
 echo Installing dependencies.
 haxelib install lime
 haxelib install openfl
-haxelib set flixel 5.9.0
-haxelib set flixel-addons 3.3.0
+haxelib set flixel 6.0.0
+haxelib set flixel-addons 3.3.2
 haxelib set flixel-ui 2.6.3
 haxelib install hxcpp
 haxelib install parallaxlt 0.0.4
