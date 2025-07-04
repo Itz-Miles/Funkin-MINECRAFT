f6942562099f3d9674b4f9e2128c58605c94a27f
Itz_MIles <95124554+Itz-Miles@users.noreply.github.com>
2025-07-03 23:56:07 -0600

    valid link

--- source/Main.hx
+++ source/Main.hx
@@ -159,7 +159,7 @@ class Main extends Sprite
 
 		errMsg += "\nUncaught Error: "
 			+ e.error
-			+ "\nPlease report this error to the GitHub page: https://github.com/Itz-Miles/Funkin-MINECRAFT\n\n> Crash Handler written by sqirra-rng";
+			+ "\nPlease report this error to the GitHub page: https://github.com/Itz-Miles/Funkin-MINECRAFT \n\n> Crash Handler written by sqirra-rng";
 
 		if (!FileSystem.exists("./crash/"))
 			FileSystem.createDirectory("./crash/");
