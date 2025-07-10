3b960066a8a3ea85b88c0c0ad39013cb5fbea3a8
Itz_Miles <itzmilesdev@gmail.com>
2025-07-09 18:06:54 -0600

    Revert "testing library versions"
    This reverts commit 793a9a4cb6b7d3d6216aa78734f12a2099c6eb86.

--- setup/unix.sh
+++ setup/unix.sh
@@ -5,12 +5,9 @@ haxelib install flixel 6.0.0
 haxelib set flixel 6.0.0
 haxelib install flixel-addons 3.3.2
 haxelib set flixel-addons 3.3.2
-haxelib install flixel-ui 2.6.4
-haxelib set flixel-ui 2.6.4
+haxelib install flixel-ui 2.6.3
+haxelib set flixel-ui 2.6.3
 haxelib install hxcpp
-haxelib set hxcpp 4.3.0
-haxelib install parallaxlt 0.0.5
+haxelib install parallaxlt 0.0.4
 haxelib install hxcpp-debug-server
-haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp v4.3.91ub.com/Haxhaxelib eFoundation/hxcpp v4.3.91
-haxelib set hxcpp git
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
--- setup/windows.bat
+++ setup/windows.bat
@@ -10,15 +10,12 @@ haxelib set flixel 6.0.0
 haxelib install flixel-addons 3.3.2
 haxelib install flixel-addons 3.3.2
 haxelib set flixel-addons 3.3.2
-haxelib install flixel-ui 2.6.4
-haxelib set flixel-ui 2.6.4
+haxelib install flixel-ui 2.6.3
+haxelib set flixel-ui 2.6.3
 haxelib install hxcpp
-haxelib set hxcpp 4.3.0
-haxelib install parallaxlt 0.0.5
+haxelib install parallaxlt 0.0.4
 haxelib install hxcpp-debug-server
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
-haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp v4.3.91ub.com/Haxhaxelib eFoundation/hxcpp v4.3.91
-haxelib set hxcpp git
 curl -# -O https://download.visualstudio.microsoft.com/download/pr/3105fcfe-e771-41d6-9a1c-fc971e7d03a7/8eb13958dc429a6e6f7e0d6704d43a55f18d02a253608351b6bf6723ffdaf24e/vs_Community.exe
 vs_Community.exe --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK.19041 -p
 del vs_Community.exe
