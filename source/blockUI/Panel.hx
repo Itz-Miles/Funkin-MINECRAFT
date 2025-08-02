98aaf8ee7673b367350c780b491186eef825993d
Itz_Miles <itzmilesdev@gmail.com>
2025-08-02 10:10:57 -0600

    we are adding text to this

--- source/blockUI/Panel.hx
+++ source/blockUI/Panel.hx
@@ -1,5 +1,6 @@
 package blockUI;
 
+import flixel.text.FlxText;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import flixel.math.FlxRect;
@@ -13,7 +14,7 @@ class Panel extends FlxSpriteGroup
 {
 	/**
 	 * Constructs the panel using an array of layer definitions.
-	 * @param layers Array of Layers (x, y, width, height, color).
+	 * @param layers Array of Layers (x, y, width, height, color, text, font, size).
 	 */
 	public function new(layers:Array<Layer>)
 	{
@@ -21,14 +22,26 @@ class Panel extends FlxSpriteGroup
 
 		for (layer in layers)
 		{
-			var sprite = new FlxSprite();
+			if (layer.text == null)
+			{
+				var sprite = new FlxSprite();
 
-			sprite.makeGraphic(1, 1, layer.color);
-			sprite.setPosition(layer.x, layer.y);
-			sprite.scale.set(layer.width, layer.height);
-			sprite.updateHitbox();
+				sprite.makeGraphic(1, 1, layer.color);
+				sprite.setPosition(layer.x, layer.y);
+				sprite.scale.set(layer.width, layer.height);
+				sprite.updateHitbox();
 
-			add(sprite);
+				add(sprite);
+			}
+			else
+			{
+				var text:FlxText = new FlxText(layer.x, layer.y, layer.width);
+
+				text.setFormat(Paths.font(layer.font), layer.size, layer.color);
+				text.text = layer.text;
+				
+				add(text);
+			}
 		}
 	}
 }
