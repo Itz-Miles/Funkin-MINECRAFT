e6d9edc0441ae7647a793188cf876ac6e6ac02d0
Itz_Miles <itzmilesdev@gmail.com>
2025-08-14 12:56:20 -0600

    let's start with a panel

--- source/editors/TextChartingState.hx
+++ source/editors/TextChartingState.hx
@@ -1,9 +1,42 @@
 package editors;
 
+import blockUI.Layer;
+import blockUI.Panel;
 import flixel.FlxG;
 
 class TestChartingState extends MusicBeatState
 {
+	var panelLayers:Array<Layer> = [
+		{
+			x: 460,
+			y: 660,
+			width: 770,
+			height: 10,
+			color: 0xff000000
+		},
+		{
+			x: 460,
+			y: 50,
+			width: 770,
+			height: 610,
+			color: 0xff353535
+		},
+		{
+			x: 470,
+			y: 60,
+			width: 750,
+			height: 590,
+			color: 0xFF0f0f0f
+		},
+		{
+			x: 470,
+			y: 60,
+			width: 750,
+			height: 10,
+			color: 0xff000000
+		}
+	];
+
 	override function create()
 	{
 		Paths.clearStoredMemory();
@@ -11,6 +44,9 @@ class TestChartingState extends MusicBeatState
 
 		if (FlxG.sound.music != null)
 			FlxG.sound.music.stop();
+
+		var panel:Panel = new Panel(panelLayers);
+		add(panel);
 	}
 
 	override function update(elapsed:Float)
