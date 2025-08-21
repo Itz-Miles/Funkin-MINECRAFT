e9bdc6bf5bd116101f9776b7942bd7a12b0b2ae3
Itz_Miles <itzmilesdev@gmail.com>
2025-08-21 09:38:00 -0600

    leveldata typedef

--- source/Level.hx
+++ source/Level.hx
@@ -1,7 +1,39 @@
+import flixel.util.FlxColor;
+
 class Level
 {
 	/**
 	 * A list of the level directories found in the filesystem.
 	 */
 	public static var levelsList:Array<String>;
+}
+
+typedef LevelData =
+{
+	var chunks:Array<Array<Chunk>>;
+}
+
+typedef Chunk =
+{
+	/**
+	 * A list of this chunk's level objects.
+	 */
+	var objects:Array<LevelObject>;
+	/**
+	 * OPTIONAL: This chunk's bgColor to fade into.
+	 */
+	var ?bgColor:FlxColor;
+}
+
+typedef LevelObject =
+{
+	/**
+	 * Releative x coordinate.
+	 */
+	var ?x:Int;
+
+	/**
+	 * Releative y coordinate.
+	 */
+	var ?y:Int;
 }
