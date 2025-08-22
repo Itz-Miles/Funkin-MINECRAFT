bbc7811d36bb7d78286f1f76843ca16839cc3a50
Itz_Miles <itzmilesdev@gmail.com>
2025-08-21 18:31:26 -0600

    apparently html5 supports ogg fine

--- source/Paths.hx
+++ source/Paths.hx
@@ -14,7 +14,7 @@ using StringTools;
 
 class Paths
 {
-	inline public static var SOUND_EXT = #if web "mp3" #else "ogg" #end;
+	inline public static var SOUND_EXT = "ogg";
 
 	public static function excludeAsset(key:String)
 	{
