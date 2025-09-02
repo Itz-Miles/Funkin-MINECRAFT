bba6deb7022b1461bbbd74e18ca65e46b29eadef
Itz_Miles <itzmilesdev@gmail.com>
2025-09-02 17:59:16 -0600

    a mod editor of sorts

---
+++ source/editors/ModEditor.hx
@@ -0,0 +1,17 @@
+package editors;
+
+/**
+ * The Mod Editor serves as a hub for adding and creating mods.
+ */
+class ModEditor extends MusicBeatState
+{
+	override function create()
+	{
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
