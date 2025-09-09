6e6ea1c42674739fa72453e82491f19b040df524
Itz_Miles <itzmilesdev@gmail.com>
2025-09-08 22:48:05 -0600

    menus will be flxcontainers for layering purposes

---
+++ source/menus/Menu.hx
@@ -0,0 +1,17 @@
+package menus;
+
+import flixel.group.FlxContainer;
+
+/**
+ * The Menu class
+ */
+class Menu extends FlxContainer
+{
+	public function create()
+	{
+	}
+
+	public function refresh()
+	{
+	}
+}
