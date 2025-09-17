ff6800db9e20083d3276273feb3288a449e7d230
Itz_Miles <itzmilesdev@gmail.com>
2025-09-17 17:35:09 -0600

    button and slider

---
+++ source/blockUI/Button.hx
@@ -0,0 +1,11 @@
+package blockUI;
+
+class Button
+{
+	/**
+	 * Makes (or recycles) a Button.
+	 */
+	public static function make(x:Int, y:Int, width:Int, height:Int)
+	{
+	}
+}
---
+++ source/blockUI/Slider.hx
@@ -0,0 +1,12 @@
+package blockUI;
+
+class Slider
+{
+	/**
+	 * Makes (or recycles) a Slider.
+	 */
+	public static function make(x:Int, y:Int, width:Int, height:Int)
+	{
+
+	}
+}
