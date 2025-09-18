f33c717be9464d3c85d0d30aeaac1c324992ae4e
Itz_Miles <itzmilesdev@gmail.com>
2025-09-17 19:52:42 -0600

    i mean we're starting something

--- source/blockUI/Button.hx
+++ source/blockUI/Button.hx
@@ -1,11 +1,57 @@
 package blockUI;
 
-class Button
+class Button extends FlxSpriteContainer
 {
+	private static var cache:FlxTypedGroup<Button>;
+
+	private var outline:FlxSprite;
+
+	private var rim:FlxSprite;
+
+	private var border:FlxSprite;
+
+	private var face:FlxSprite;
+
+	private var label:FlxText;
+
 	/**
 	 * Makes (or recycles) a Button.
 	 */
-	public static function make(x:Int, y:Int, width:Int, height:Int)
+	public static function make(X:Int, Y:Int, Width:Int, Height:Int, BorderSize:Int, RimSize:Int, Label:String, Color:Int, LabelColor:Int):Button
 	{
+		var button:Button = cache.recycle(Button, Button.new);
+
+		return button;
+	}
+
+	public function new()
+	{
+		super();
+
+		init();
+	}
+
+	function init()
+	{
+		outline = new FlxSprite().makeGraphic(1, 1);
+		rim = new FlxSprite().makeGraphic(1, 1);
+		border = new FlxSprite().makeGraphic(1, 1);
+		face = new FlxSprite().makeGraphic(1, 1);
+		label = new FlxText();
+
+		add(outline);
+		add(rim);
+		add(border);
+		add(face);
+		add(label);
+	}
+
+	function setup(X:Int, Y:Int, Width:Int, Height:Int, BorderSize:Int, RimSize:Int, Label:String, Color:Int, LabelColor:Int)
+	{
+		x = X;
+		y = Y;
+
+		outline.setPosition(X - BorderSize, Y - BorderSize);
+		outline.setGraphicSize(Width + BorderSize * 2, Height + BorderSize * 2);
 	}
 }
