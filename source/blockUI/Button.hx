78e460d799f2787fc7e9fdf77948c22fecf39a39
Itz_Miles <itzmilesdev@gmail.com>
2025-09-18 09:18:12 -0600

    i am getting very annoyed

--- source/blockUI/Button.hx
+++ source/blockUI/Button.hx
@@ -2,7 +2,7 @@ package blockUI;
 
 class Button extends FlxSpriteContainer
 {
-	private static var cache:FlxTypedGroup<Button>;
+	private static var cache:FlxTypedGroup<Button> = new FlxTypedGroup<Button>();
 
 	private var outline:FlxSprite;
 
@@ -17,9 +17,11 @@ class Button extends FlxSpriteContainer
 	/**
 	 * Makes (or recycles) a Button.
 	 */
-	public static function make(X:Int, Y:Int, Width:Int, Height:Int, BorderSize:Int, RimSize:Int, Label:String, Color:Int, LabelColor:Int):Button
+	public static function make(X:Int, Y:Int, Width:Int, Height:Int, BorderSize:Int, RimSize:Int, Label:String, ?Color:Int = FlxColor.GRAY, ?LabelColor:Int = FlxColor.WHITE, ?OutlineColor:Int = FlxColor.BLACK):Button
 	{
 		var button:Button = cache.recycle(Button, Button.new);
+
+		button.setup(X, Y, Width, Height, BorderSize, RimSize, Label, Color, LabelColor, OutlineColor);
 
 		return button;
 	}
@@ -46,12 +48,45 @@ class Button extends FlxSpriteContainer
 		add(label);
 	}
 
-	function setup(X:Int, Y:Int, Width:Int, Height:Int, BorderSize:Int, RimSize:Int, Label:String, Color:Int, LabelColor:Int)
+	function setup(X:Int, Y:Int, Width:Int, Height:Int, BorderSize:Int, RimSize:Int, Label:String, Color:Int, LabelColor:Int, OutlineColor:Int)
 	{
+		// outline.setPosition(-BorderSize, -BorderSize);
+		outline.setGraphicSize(Width + (BorderSize * 2), Height + (BorderSize * 2));
+		outline.color = OutlineColor;
+
+		rim.setGraphicSize(Width, Height);
+		rim.color = FlxColor.fromInt(Color).getDarkened(0.3);
+
+		// border.y -= RimSize;
+		border.setGraphicSize(Width, Height);
+		border.color = FlxColor.fromInt(Color).getLightened(0.1);
+
+		// face.x += BorderSize;
+		// face.y += BorderSize - RimSize;
+		face.setGraphicSize(Width - (BorderSize * 2), Height - (BorderSize * 2));
+		face.color = Color;
+
+		// label.x += BorderSize * 4;
+		var fntSize = Std.int(Math.min((Width - BorderSize * 4) / (Label.length * 0.8), (Height - 4 - BorderSize * 4) * 0.75));
+		// label.y += BorderSize * 2 + ((Height - BorderSize * 4) * 0.5) - Std.int(Math.min((((Width - BorderSize * 4) / (Label.length * 0.8) + 4) / 3) * 4, (Height - BorderSize * 4)) * 0.5);
+		label.setFormat(Paths.font("Monocraft.ttf"), fntSize, LabelColor, CENTER);
+		label.text = Label;
+
 		x = X;
 		y = Y;
+		scrollFactor.set();
+	}
 
-		outline.setPosition(X - BorderSize, Y - BorderSize);
-		outline.setGraphicSize(Width + BorderSize * 2, Height + BorderSize * 2);
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+		if (Controls.UI_LEFT)
+			x -= 100 * elapsed;
+		if (Controls.UI_RIGHT)
+			x += 100 * elapsed;
+		if (Controls.UI_UP)
+			y -= 100 * elapsed;
+		if (Controls.UI_DOWN)
+			y += 100 * elapsed;
 	}
 }
--- source/menus/StoryMenu.hx
+++ source/menus/StoryMenu.hx
@@ -2,16 +2,22 @@ package menus;
 
 class StoryMenu extends Menu
 {
+	var button:Button;
+
 	override public function create()
 	{
 		super.create();
 		header = new Panel(LayerData.HEADER);
 		header.text = "understand the stories";
 		add(header);
+
+		button = Button.make(100, 200, 200, 100, 5, 10, "woah", null, 0xFF000000);
+		add(button);
 	}
 
 	override public function refresh()
 	{
+		trace(button.x);
 		header.runAcrossLayers(0);
 		super.refresh();
 	}
