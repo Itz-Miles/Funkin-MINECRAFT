9fff851533154624a15351a26cfd6ff253763eb1
Itz_Miles <itzmilesdev@gmail.com>
2025-09-18 18:25:15 -0600

    hooray

--- source/blockUI/Button.hx
+++ source/blockUI/Button.hx
@@ -1,6 +1,6 @@
 package blockUI;
 
-class Button extends FlxContainer
+class Button extends FlxSpriteContainer
 {
 	private static var cache:FlxTypedGroup<Button> = new FlxTypedGroup<Button>();
 
@@ -36,6 +36,8 @@ class Button extends FlxContainer
 
 	function init()
 	{
+		scrollFactor.set();
+
 		outline = new FlxSprite().makeGraphic(1, 1);
 		outline.scrollFactor.set();
 
@@ -60,31 +62,33 @@ class Button extends FlxContainer
 
 	function setup(X:Int, Y:Int, Width:Int, Height:Int, BorderSize:Int, RimSize:Int, Label:String, Color:Int, LabelColor:Int, OutlineColor:Int)
 	{
-		outline.setPosition(X - BorderSize, Y - BorderSize);
+		outline.setPosition(-BorderSize, -BorderSize);
 		outline.setGraphicSize(Width + (BorderSize * 2), Height + (BorderSize * 2));
 		outline.updateHitbox();
 		outline.color = OutlineColor;
 
-		rim.setPosition(X, Y);
+		rim.setPosition(0, 0);
 		rim.setGraphicSize(Width, Height);
 		rim.updateHitbox();
 		rim.color = FlxColor.fromInt(Color).getDarkened(0.3);
 
-		border.setPosition(X, Y - RimSize);
+		border.setPosition(0, -RimSize);
 		border.setGraphicSize(Width, Height);
 		border.updateHitbox();
 		border.color = FlxColor.fromInt(Color).getLightened(0.1);
 
-		face.setPosition(X + BorderSize, Y + BorderSize - RimSize);
+		face.setPosition(BorderSize, BorderSize - RimSize);
 		face.setGraphicSize(Width - (BorderSize * 2), Height - (BorderSize * 2));
 		face.updateHitbox();
 		face.color = Color;
 
-		label.setPosition(X + (BorderSize * 2), Y - RimSize + (BorderSize * 2));
+		label.setPosition(BorderSize * 2, -RimSize + (BorderSize * 2));
 		label.fieldWidth = Width - BorderSize - 4;
 		var fntSize = Std.int(Math.min((Width - BorderSize * 4) / (Label.length * 0.8), (Height - 4 - BorderSize * 4) * 0.75));
 		// label.y += BorderSize * 2 + ((Height - BorderSize * 4) * 0.5) - Std.int(Math.min((((Width - BorderSize * 4) / (Label.length * 0.8) + 4) / 3) * 4, (Height - BorderSize * 4)) * 0.5);
 		label.setFormat(Paths.font("Monocraft.ttf"), fntSize, LabelColor, CENTER);
 		label.text = Label;
+
+		setPosition(X, Y);
 	}
 }
